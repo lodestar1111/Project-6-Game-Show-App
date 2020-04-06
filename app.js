4acd71921c3139c62835069f87ec3df598bff11e
lodestar1111 <60457900+lodestar1111@users.noreply.github.com>
2020-04-06 12:05:30 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,54 @@
 // Game Variables
-const qwerty = document.getElementById('qwerty');
-const phrase = document.getElementById('phrase');
-const startButton = document.querySelectorAll('.btn__reset');
-const buttons = document.querySelectorAll('.keyrow button');
+const qwerty = document.getElementById("qwerty");
+const phrase = document.getElementById("phrase");
+const startButton = document.querySelector(".btn__reset");
+const buttons = document.querySelectorAll(".keyrow, button");
 let missed = 0;
-let lives = document.querySelectorAll('.tries');
+let lives = document.querySelectorAll(".tries");
 // Phrases
-function startGame() {
-    let phrases = [
-        "Hold my beer",
-        "Do not grow up it is a trap",
-        "Of course I talk to myself",
-        "So many cats so little time",
-        "Humpty Dumpty was pushed",
-        "Staying home saves lives",
-        "This button has no funny phrase in it",
-        "May I see your manager"
-    ];
 
-    startButton.addEventListener("click", () =>
-        document.getElementById("overlay").style.display = "none");
+let phrases = [
+  "Hold my beer",
+  "Do not grow up it is a trap",
+  "Of course I talk to myself",
+  "So many cats so little time",
+  "Humpty Dumpty was pushed",
+  "Staying home saves lives",
+  "This button has no funny phrase in it",
+  "May I see your manager",
+];
 
-    //return a random phrase from an array
-    function getRandomPhraseAsArray(arr) {
-        let randomNumber = Math.floor(Math.random() * arr.length);
-        let split = arr[randomNumber].split('');
-        return split;
-    }
+startButton.addEventListener("click", () => {
+  document.getElementById("overlay").style.display = "none";
+});
 
-    getRandomPhraseAsArray(phrases);
+//return a random phrase from an array
+function getRandomPhraseAsArray(arr) {
+  let randomNumber = Math.floor(Math.random() * arr.length);
+  let split = arr[randomNumber].split("");
+  return split;
+}
 
-    function addPhraseToDisplay(arr) {
+functiongetRandomPhraseAsArray(phrases);
 
+//adds the letters of a string to the display
+function addPhraseToDisplay(arr) => {
+    
+};
 
+//check if a letter is in the phrase
+const checkLetter  (button) => {
+ let li = document.querySelectorAll('.letter'); 
+};
 
+//check if the game has been won or lost
+const checkWin = () => {};
 
+//listen for the start game button to be pressed
+startButton.addEventListener('click', () => {
 
-        //adds the letters of a string to the display
-        const addPhraseToDisplay = arr => {
+});
+// listen for the onscreen keyboard to be clicked
+qwerty.addEventListener('click', e => {
 
-
-        }
-
-        //check if a letter is in the phrase
-        const checkLetter = button => {
-
-
-        }
-
-        //check if the game has been won or lost
-        const checkWin = () => {
-
-
-        }
-
-        //listen for the start game button to be pressed
-
-
-
-        // listen for the onscreen keyboard to be clicked
+});
